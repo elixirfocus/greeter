50b94a7701b61179423e819adeb8314bd000d055
Mike Zornek <zorn@zornlabs.com>
2020-10-18 15:26:30 -0400

    Avoid alias to simplify code.

--- test/greeter/name_formatter_test.exs
+++ test/greeter/name_formatter_test.exs
@@ -1,11 +1,9 @@
 defmodule Greeter.NameFormatterTest do
   use ExUnit.Case, async: true
 
-  alias Greeter.NameFormatter
-
   describe "format/1" do
     test "works with simple name" do
-      assert NameFormatter.format("mike") == "Mike"
+      assert Greeter.NameFormatter.format("mike") == "Mike"
     end
   end
 end
